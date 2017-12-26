467c99ce5e90eca3ae6b510ea8c2fbeb51f8fa94
phost1111 <phost1111@gmail.com>
2017-12-26 17:37:59 +0100

    adding LoL current match lookup and dependency to 'cassiopeia'

--- bot.py
+++ bot.py
@@ -12,11 +12,12 @@ with open('leaguekey.txt', 'r') as leaguekeyfile:
     leaguekey = leaguekeyfile.readline()
 cass.set_riot_api_key(key=leaguekey)
 
-VERSION = '0.1.0a'
+VERSION = '0.2.0a'
 ADMINS = ['139354514091147264']
 PREFIX = '!'
 ADMINPREFIX = '*'
 DELETIONWAITTIME = 5
+
 
 @client.event
 async def on_ready():
@@ -25,6 +26,7 @@ async def on_ready():
     print(client.user.id)
     print('------')
     await changePlaying('you suffer', 3)
+
 
 @client.event
 async def on_message(message):
@@ -46,30 +48,8 @@ async def on_message(message):
     if (message.content.startswith(PREFIX)) & (message.author.id != client.user.id):
         message.content = message.content[len(PREFIX):]
         args = message.content.split()
-        if (args[0] == 'test') & (len(args) > 2):
-            tmpmessage = await  sendMessage(message.channel, "Fetchig data from Riot's Servers...")
-            tmpregion = args[1]
-            args.pop(0)
-            args.pop(0)
-            tmpsummonername = ' '.join(args)
-            summoner = cass.get_summoner(name=tmpsummonername, region=tmpregion)
-            match = summoner.current_match
-            blueteam = match.blue_team.participants
-            redteam = match.red_team.participants
-            players = 'Blue Team:```\n'
-            for player in blueteam:
-                for mastery in player.summoner.champion_masteries:
-                    if mastery.champion.id == player.champion.id:
-                        players += player.summoner.name + ' (Lvl ' + str(mastery.level) + ' ' + player.champion.name + ')' + '\n'
-                        return
-            players += '```Red Team:```\n'
-            for player in redteam:
-                for mastery in player.summoner.champion_masteries:
-                    if mastery.champion.id == player.champion.id:
-                        players += player.summoner.name + ' (Lvl ' + str(mastery.level) + ' ' + player.champion.name + ')' + '\n'
-                        return
-            players += '```'
-            await client.edit_message(tmpmessage, new_content=players)
+        if (args[0] == 'test'):
+            await sendMessage(message.channel, 'Nothing to test :)')
         elif args[0] == 'msgcount':
             counter = 0
             tmp = await sendMessage(message.channel, 'Calculating...')
@@ -95,7 +75,8 @@ async def on_message(message):
             if not tmpisadmin:
                 await sendAndDeleteMessages(message, message.channel, 'You are not a PhostBotAdmin!')
         elif args[0] == 'help':
-            await sendMessage(message.channel, ('```' + PREFIX + 'help - show all commands\n' + PREFIX + 'msgcount - How many messages have you sent in this channel? (Max 100)\n' + PREFIX + 'version - Which version is the bot on?\n' + PREFIX + 'clear [amount] - clears the last [amount]/100 messages\n' + PREFIX + 'registerCrossServer - registers the channel this command is issued in to our Cross-Server-System\n' + PREFIX + 'removeCrossServer - removes the channel this command is issued in from our Cross-Server-System\n' + '```'))
+            await sendMessage(message.channel, (
+            '```' + PREFIX + 'help - show all commands\n' + PREFIX + 'msgcount - How many messages have you sent in this channel? (Max 100)\n' + PREFIX + 'version - Which version is the bot on?\n' + PREFIX + 'clear [amount] - clears the last [amount]/100 messages\n' + PREFIX + 'registerCrossServer - registers the channel this command is issued in to our Cross-Server-System\n' + PREFIX + 'removeCrossServer - removes the channel this command is issued in from our Cross-Server-System\n' + PREFIX + 'lol match (REGION) (summoner) - get the players in your current LoL match\n' + '```'))
         elif args[0] == 'registerCrossServer':
             tmpisadmin = False
             for role in message.author.roles:
@@ -113,7 +94,7 @@ async def on_message(message):
                 await sendAndDeleteMessages(message, message.channel, 'That channel is already registered!')
                 return
             with open('crossserverchannels.txt', 'a') as file:
-                file.write(message.channel.id + '\n')
+                file.write('\n' + message.channel.id)
             CROSSSERVERCHANNELS.append(message.channel.id)
             await sendMessage(message.channel, 'Successfully registered this channel for CrossServer use!')
         elif args[0] == 'removeCrossServer':
@@ -128,6 +109,37 @@ async def on_message(message):
             with open('crossserverchannels.txt', 'w') as file:
                 file.write('\n'.join(CROSSSERVERCHANNELS))
             await sendMessage(message.channel, 'Successfully removed this channel from CrossServer use!')
+        elif (args[0] == 'lol') & (args[1] == 'match') & (len(args) > 3):
+            tmpregion = args[2]
+            args.pop(0)
+            args.pop(0)
+            args.pop(0)
+            tmpsummonername = ' '.join(args)
+            summoner = cass.get_summoner(name=tmpsummonername, region=tmpregion)
+            match = summoner.current_match
+            tmpmessage = await  sendMessage(message.channel, "Fetchig data from Riot's Servers...")
+            blueteam = match.blue_team.participants
+            redteam = match.red_team.participants
+            outputtext = 'Blue Team:\n```'
+            for player in blueteam:
+                tmprank = 'Unranked'
+                for leagueposition in player.summoner.league_positions:
+                    if leagueposition.queue.value == 'RANKED_SOLO_5x5':
+                        tmprank = leagueposition.tier.value + ' ' + leagueposition.division.value
+                        break
+                outputtext += player.summoner.name + ' (' + player.champion.name + '): ' + 'Level ' + str(
+                    player.summoner.level) + ' ' + tmprank + '\n'
+            outputtext += '```Red Team:```\n'
+            for player in redteam:
+                tmprank = 'Unranked'
+                for leagueposition in player.summoner.league_positions:
+                    if leagueposition.queue.value == 'RANKED_SOLO_5x5':
+                        tmprank = leagueposition.tier.value + ' ' + leagueposition.division.value
+                        break
+                outputtext += player.summoner.name + ' (' + player.champion.name + '): ' + 'Level ' + str(
+                    player.summoner.level) + ' ' + tmprank + '\n'
+            outputtext += '```'
+            await client.edit_message(tmpmessage, outputtext)
         return
     if message.author.id != client.user.id:
         for channelid in CROSSSERVERCHANNELS:
@@ -135,23 +147,26 @@ async def on_message(message):
                 tmpcrossserverchannels = CROSSSERVERCHANNELS.copy()
                 tmpcrossserverchannels.remove(channelid)
                 for channelid2 in tmpcrossserverchannels:
-                    tmpembed = discord.Embed(type='rich', description=message.content).set_author(name='@' + message.author.name + '#' + message.author.discriminator, icon_url=message.author.avatar_url)
-                    await client.send_message (client.get_channel(channelid2), embed=tmpembed)
-
-
+                    tmpembed = discord.Embed(type='rich', description=message.content).set_author(
+                        name='@' + message.author.name + '#' + message.author.discriminator,
+                        icon_url=message.author.avatar_url)
+                    await client.send_message(client.get_channel(channelid2), embed=tmpembed)
 
 
 async def changePlaying(Text, Type):
     await client.change_presence(game=discord.Game(name=Text, type=Type))
 
+
 async def sendMessage(channel, messageText):
     await client.send_typing(channel)
     return await client.send_message(channel, messageText)
+
 
 async def sendAndDeleteMessages(message, channel, messageText):
     tmpmessages = [await sendMessage(channel, messageText), message]
     await asyncio.sleep(DELETIONWAITTIME)
     await client.delete_messages(tmpmessages)
+
 
 open('token.txt', 'a').close()
 open('crossserverchannels.txt', 'a').close()
